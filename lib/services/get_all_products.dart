78337d14ff28561e0051ad26f6d2d89410289642
Ziad Salah <zslah1935@gmail.com>
2024-06-10 12:29:08 +0300

    get all products and add http package

--- lib/services/get_all_products.dart
+++ lib/services/get_all_products.dart
@@ -1,1 +1,17 @@
-class GetAllProducts {}
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:store_application/models/product_model.dart';
+
+class GetAllProducts {
+  Future<List<ProductModel>> getAllProducts() async {
+    http.Response response =
+        await http.get(Uri.parse('https://fakestoreapi.com/products'));
+    List<dynamic> data = jsonDecode(response.body);
+    List<ProductModel> allProducts = [];
+    for (int i = 0; i < data.length; i++) {
+      allProducts.add(ProductModel.fromJson(data[i]));
+    }
+    return allProducts;
+  }
+}
